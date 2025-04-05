3d58be3e1fec8c98ab76caf0a79e587d317fa112
Burekslayer <ivanscvetkovic@gmail.com>
2025-04-05 19:22:58 +0200

    deleted unused variables

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 // src/pages/RegisterPage.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useSearchParams, useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
   const [firstName, setFirstName] = useState('');
